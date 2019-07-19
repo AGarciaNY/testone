00a679261e15d81da9da76b00500b4c3989508a9
AGarciaNY <aldairgarcia803@gmail.com>
2019-07-18 22:01:56 -0400

    add nav

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import './App.css';
-import styled from '@emotion/styled';
 import Nav from '../Nav/Nav'
-const Div =styled.div`
-background-color:red;
-`
+
 function App() {
   return (
-    <Div >
-      <h1>hi there</h1>
       <Nav/>
-    </Div>
   );
 }
 
--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import './Nav.css';
 function Nav() {
   return (
-    <div>
-        <h1>hi there test</h1>
+    <div className="navcontainer">
+      <div>
+        <h1 className="myname">Aldair's Portfolio</h1>
+      </div>
+      <div className="links">
+          <div >
+            <a className="linkspace">Home</a>
+            <a className="linkspace">About</a>
+            <a className="linkspace">Projects</a>
+          </div>
+          <div className="dropdown linkspace">
+              <button className="dropbutton">Dropdown</button>
+              <div className="dropdown-content">
+                  <a href="#">Link 1</a>
+                  <a href="#">Link 2</a>
+                  <a href="#">Link 3</a>
+              </div>
+          </div>
+      </div>
     </div>
   );
 }
