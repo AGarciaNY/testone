440fa3f52365607cc6004b9311aa2aca0677c703
AGarciaNY <aldairgarcia803@gmail.com>
2019-07-19 20:04:08 -0400

    sadasd

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Nav from '../Nav/Nav'
-
+import Footer from '../Footer/Footer'
 function App() {
   return (
-      <Nav/>
+  <div>
+    <Nav/>
+    <Footer/>
+  </div>
   );
 }
 
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import styled from '@emotion/styled';
-const Div =styled.div`
-background-color:red;
-`
+//import styled from '@emotion/styled';
+import './Footer.css';
 function Footer() {
   return (
-    <Div >
-      <h1>hi there</h1>
-    </Div>
+    <div id="Footer">
+      <div>
+        <h1>More Informtion</h1>
+      </div>
+      <div>
+        <div>
+          <h1>Contact Informtion</h1>
+          <h1></h1>
+        </div>
+
+      </div>
+    </div>
   );
 }
 
