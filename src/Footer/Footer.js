dec195563abdab4de0b9130f57a6873bab1f2280
AGarciaNY <aldairgarcia803@gmail.com>
2019-07-22 12:48:12 -0400

    footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,15 +4,23 @@ import './Footer.css';
 function Footer() {
   return (
     <div id="Footer">
-      <div>
-        <h1>More Informtion</h1>
+      <div id="HeadingCon">
+        <h1 id="Footerheading">More Informtion</h1>
       </div>
       <div>
-        <div>
-          <h1>Contact Informtion</h1>
-          <h1></h1>
+        <div >
+          <h1 class="Contact">Contact Informtion</h1>
+          <ul class="ContactI" id="Information">
+            <li className="Conlist">
+              <img src="/img"/>
+              <h1 class="ContactI">Aldairgarcia360@gmail.com</h1>
+            </li>
+            <li className="Conlist">
+              <img src="/img"/>
+              <h1 class="ContactI">AGarciaNY</h1>
+            </li>
+          </ul>
         </div>
-
       </div>
     </div>
   );
